2986698e3193d3630becb8002d5cf64cfebe8911
Vavong <alexis.itvg@gmail.com>
2026-04-23 22:55:30 +0200

    Feat: upgrade cpp_09/ex02

--- cpp_09/ex02/src/PmergeMe.cpp
+++ cpp_09/ex02/src/PmergeMe.cpp
@@ -31,8 +31,13 @@ void	PmergeMe::sortWithVector(std::vector<int> &seq) {
 	
 	std::vector<int>	winners;
 	std::vector<int>	losers;
+	int straggler = 	-1;
+	bool hasStraggler = false;
 
-	for (int i = 0; i < seq.size(); i += 2) {
+	if (seq.size() <= 1)
+		return;
+
+	for (size_t i = 0; i + 1 < seq.size(); i += 2) {
 		
 		if (seq[i] > seq[i + 1]) {
 			winners.push_back(seq[i]);
@@ -45,16 +50,24 @@ void	PmergeMe::sortWithVector(std::vector<int> &seq) {
 	}
 
 	if (seq.size() % 2 != 0) {
-		(seq.size() - 1);
+		straggler = seq[seq.size() - 1];
+		hasStraggler = true;
 	}
+
+	sortWithVector(winners);
 }
 
 void	PmergeMe::sortWithDeque(std::deque<int> &seq) {
 	
-	std::deque<int>	winners;
-	std::deque<int>	losers;
+	std::deque<int>		winners;
+	std::deque<int>		losers;
+	int straggler = 	-1;
+	bool hasStraggler = false;
 
-	for (int i = 0; i < seq.size(); i += 2) {
+	if (seq.size() <= 1)
+		return;
+
+	for (size_t i = 0; i + 1 < seq.size(); i += 2) {
 		
 		if (seq[i] > seq[i + 1]) {
 			winners.push_back(seq[i]);
@@ -67,6 +80,9 @@ void	PmergeMe::sortWithDeque(std::deque<int> &seq) {
 	}
 
 	if (seq.size() % 2 != 0) {
-		(seq.size() - 1);
+		straggler = seq[seq.size() - 1];
+		hasStraggler = true;
 	}
+
+	sortWithDeque(winners);
 }
